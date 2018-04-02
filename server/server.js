b10aa1ea1414105722a8a748190152a5762a8c55
DanielMarcano <danielemarcano96@gmail.com>
2018-04-02 16:07:30 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,12 +13,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res, next) => {
+app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
   });
   todo.save().then(doc => res.status(201).send(doc), e => res.status(400).send(e.message));
 })
+  .get('/todos', (req, res) => {
+    Todo.find().then(todos => res.status(200).send({ todos }), e => res.status(400).send(e));
+  })
   .listen(port, () => console.log(`Server up and running at ${port}`));
 
 module.exports = { app };
