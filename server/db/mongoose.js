662875f1b5896522c80d3d1675c1308257e9c402
DanielMarcano <danielemarcano96@gmail.com>
2018-04-02 22:55:58 +0200

    Add remote MongoDB database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://dbdan:123456@ds231529.mlab.com:31529/todo-app');
 
 module.exports = { mongoose };
